cd993a8362a357bd16d882a1c0effeadc1c70980
Dani <d.tarazoma@geekshubs.com>
2022-06-01 13:14:30 +0200

    feat: register user with JWT

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');;
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const register = async (req, res) => {
     try {
@@ -29,10 +30,17 @@ const register = async (req, res) => {
 
         const newUser = await user.save();
 
+        const token = jwt.sign({ user: newUser }, process.env.JWT_SECRET_KEY,
+            { expiresIn: 60 * 60 * 24 }
+        );
+
+        console.log(token);
+
         return res.json({
             success: true,
             message: 'User created successfully',
-            data: newUser
+            data: newUser,
+            token
         });
     } catch (error) {
         return res.json({
