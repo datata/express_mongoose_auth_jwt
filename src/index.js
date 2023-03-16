b378b106c4dbe6e8cfe071c77a033648332e2202
Dani <d.tarazoma@geekshubs.com>
2023-03-16 10:08:32 +0100

    feat: add cors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
+
 require('dotenv').config();
 
 const app = express();
@@ -9,6 +11,15 @@ const port = process.env.PORT || 3000;
 
 app.use(morgan('dev'));
 app.use(express.json());
+
+let corsOptions = {//CONFIGURO OPCIONES DE CORS
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+  optionsSuccessStatus: 204
+};
+
+app.use(cors(corsOptions));
 
 app.use('/api', require('./routes/user.routes'));
 
